2d8c385e02eb25e6ff5eb867462c09154fcf2b81
Wouter R <gennerate@zonnet.nl>
2013-12-20 16:31:41 +0100

    Add license header to source files.

--- js_src/SettingsWindow.js
+++ js_src/SettingsWindow.js
@@ -1,3 +1,11 @@
+/*
+ * This file is part of the Doodle3D project (http://doodle3d.com).
+ *
+ * Copyright (c) 2013, Doodle3D
+ * This software is licensed under the terms of the GNU GPL v2 or later.
+ * See file LICENSE.txt or visit http://www.gnu.org/licenses/gpl.html for full license details.
+ */
+
 //these settings are defined in the firmware (conf_defaults.lua) and will be initialized in loadSettings()
 var settings = { }
 
--- js_src/main.js
+++ js_src/main.js
@@ -1,3 +1,11 @@
+/*
+ * This file is part of the Doodle3D project (http://doodle3d.com).
+ *
+ * Copyright (c) 2013, Doodle3D
+ * This software is licensed under the terms of the GNU GPL v2 or later.
+ * See file LICENSE.txt or visit http://www.gnu.org/licenses/gpl.html for full license details.
+ */
+
 var debugMode = false;              // debug mode
 var sendPrintCommands = true;       // if Doodle3d should send print commands to the 3d printer
 var communicateWithWifibox = true;  // if Doodle3d should try interfacing with the wifibox (in case one is not connected)
