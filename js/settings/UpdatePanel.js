afb279fbc76f2fa0def81977a36a55e8739d6cea
Wouter R <gennerate@zonnet.nl>
2014-02-24 15:58:50 +0100

    Whitespace fixes.

--- js/settings/UpdatePanel.js
+++ js/settings/UpdatePanel.js
@@ -11,52 +11,52 @@ function UpdatePanel() {
 
 	this.wifiboxURL;
 	this.element;
-	
-	this.statusCheckInterval 	= 1000; 
-	this.statusCheckDelayer; 						// setTimout instance
-	this.installedDelay 			= 90*1000; 			// Since we can't retrieve status during installation we show the installed text after a fixed delay	
-	this.installedDelayer; 							// setTimout instance
-	this.retryDelay 					= 1000; 
-	this.retryDelayer; 									// setTimout instance
-	//this.timeoutTime 					= 3000;
-	
-	this.canUpdate 						= false;
-	this.currentVersion				= "";
-	this.newestVersion;	
+
+	this.statusCheckInterval = 1000;
+	this.statusCheckDelayer; 			// setTimout instance
+	this.installedDelay = 90*1000; 		// Since we can't retrieve status during installation we show the installed text after a fixed delay
+	this.installedDelayer; 				// setTimout instance
+	this.retryDelay = 1000;
+	this.retryDelayer; 					// setTimout instance
+	//this.timeoutTime = 3000;
+
+	this.canUpdate = false;
+	this.currentVersion = "";
+	this.newestVersion;
 	this.progress;
 	this.imageSize;
 	var _inAccessPointMode;
-	
+
 	// states from api, see Doodle3D firmware src/script/d3d-updater.lua
-	UpdatePanel.NONE 								= 1; // default state 
-  UpdatePanel.DOWNLOADING  				= 2;
-  UpdatePanel.DOWNLOAD_FAILED 		= 3;
-  UpdatePanel.IMAGE_READY 				= 4; // download successfull and checked 
-  UpdatePanel.INSTALLING 					= 5;
-  UpdatePanel.INSTALLED 					= 6;
-  UpdatePanel.INSTALL_FAILED 			= 7;
-  
-  this.state; // update state from api
-  this.stateText = ""; // update state text from api
-  
+	UpdatePanel.NONE 			= 1; // default state
+	UpdatePanel.DOWNLOADING  	= 2;
+	UpdatePanel.DOWNLOAD_FAILED	= 3;
+	UpdatePanel.IMAGE_READY 	= 4; // download successfull and checked
+	UpdatePanel.INSTALLING 		= 5;
+	UpdatePanel.INSTALLED 		= 6;
+	UpdatePanel.INSTALL_FAILED 	= 7;
+
+	this.state; // update state from api
+	this.stateText = ""; // update state text from api
+
 	var self = this;
 
 	this.init = function(wifiboxURL,updatePanelElement) {
 		_form.init(wifiboxURL,wifiboxURL,updatePanelElement);
-		
+
 		this.wifiboxURL = wifiboxURL;
-		
+
 		this.element = updatePanelElement;
 		this.retainCheckbox = this.element.find("#retainConfiguration");
 		this.includeBetasCheckbox = this.element.find("#includeBetas");
 		this.btnUpdate = this.element.find("#update");
 		this.statusDisplay = this.element.find("#updateState");
 		this.infoDisplay = this.element.find("#updateInfo");
-		
+
 		this.retainCheckbox.change(this.retainChanged);
 		this.includeBetasCheckbox.change(this.includeBetasChanged);
 		this.btnUpdate.click(this.update);
-		
+
 		this.checkStatus(false);
 	}
 	this.retainChanged = function(e) {
@@ -91,11 +91,11 @@ function UpdatePanel() {
 	}
 	this.installUpdate = function() {
 		console.log("UpdatePanel:installUpdate");
-		
+
 		// should personal sketches and settings be retained over update?
 		var retain = self.retainCheckbox.prop('checked');
 		console.log("  retain: ",retain);
-		
+
 		self.stopCheckingStatus();
 		postData = {no_retain:!retain}
 		$.ajax({
@@ -110,11 +110,11 @@ function UpdatePanel() {
 			//console.log("UpdatePanel:installUpdate: no respons (there shouldn't be)");
 		});
 		self.setState(UpdatePanel.INSTALLING);
-		
+
 		clearTimeout(self.installedDelayer);
 		self.installedDelayer = setTimeout(function() { self.setState(UpdatePanel.INSTALLED) },self.installedDelay);
 	}
-	
+
 	this.startCheckingStatus = function() {
 		clearTimeout(self.statusCheckDelayer);
 		clearTimeout(self.retryDelayer);
@@ -125,7 +125,7 @@ function UpdatePanel() {
 		clearTimeout(self.retryDelayer);
 	}
 	this.checkStatus = function(keepChecking) {
-    if (!communicateWithWifibox) return;
+		if (!communicateWithWifibox) return;
 		$.ajax({
 			url: self.wifiboxURL + "/update/status",
 			type: "GET",
@@ -133,18 +133,18 @@ function UpdatePanel() {
 			//timeout: self.timeoutTime,
 			success: function(response){
 				console.log("UpdatePanel:checkStatus response: ",response);
-				
+
 				// Keep checking ?
 				if(keepChecking) {
 					switch(self.state){
-						case UpdatePanel.DOWNLOADING: 
+						case UpdatePanel.DOWNLOADING:
 						case UpdatePanel.INSTALLING:
 							clearTimeout(self.statusCheckDelayer);
 							self.statusCheckDelayer = setTimeout(function() { self.checkStatus(keepChecking) },self.statusCheckInterval);
 							break;
 					}
 				}
-				
+
 				if(response.status != "error") {
 					var data = response.data;
 					self.handleStatusData(data);
@@ -158,25 +158,25 @@ function UpdatePanel() {
 			}
 		});
 	}
-	
+
 	this.handleStatusData = function(data) {
 		//console.log("UpdatePanel:handleStatusData");
 		var refreshUI = (self.canUpdate != data.can_update);
 
 		self.canUpdate 				= data.can_update;
-		
+
 		if(self.currentVersion != data.current_version || self.newestVersion != data.newest_version) {
 			self.currentVersion 	= data.current_version;
 			self.newestVersion 		= data.newest_version;
 			self.updateInfoDisplay();
 		}
-		
+
 		self.stateText 				= data.state_text;
 		self.progress 				= data.progress; // not always available
 		self.imageSize 				= data.image_size; // not always available
-		
+
 		self.setState(data.state_code, refreshUI);
-		
+
 		switch(this.state){
 			case UpdatePanel.IMAGE_READY:
 				self.installUpdate();
@@ -188,19 +188,19 @@ function UpdatePanel() {
 		if(!refresh && this.state == newState) return;
 		console.log("UpdatePanel:setState: ",this.state," > ",newState,"(",this.stateText,") (in Access Point Mode: ",_inAccessPointMode,") (newestVersion: ",self.newestVersion,") (refresh: ",refresh,")");
 		this.state = newState;
-		
+
 		// should personal sketches and settings be retained over update?
 		var retain = self.retainCheckbox.prop('checked');
 		//console.log("  retain", retain);
-		
+
 		// download button
-		// if there isn't newestVersion data something went wrong, 
+		// if there isn't newestVersion data something went wrong,
 		//   probably accessing the internet
 		//console.log("  self.newestVersion: ",self.newestVersion);
 		if(self.newestVersion != undefined) {
 			//console.log("  this.state: ",this.state);
 			switch(this.state){
-				case UpdatePanel.NONE: 
+				case UpdatePanel.NONE:
 				case UpdatePanel.DOWNLOAD_FAILED:
 				case UpdatePanel.INSTALL_FAILED:
 					//console.log("  self.canUpdate: ",self.canUpdate);
@@ -212,7 +212,7 @@ function UpdatePanel() {
 					break;
 				default:
 					self.btnUpdate.attr("disabled", true);
-					break;
+				break;
 			}
 		} else {
 			self.btnUpdate.attr("disabled", true);
@@ -230,23 +230,23 @@ function UpdatePanel() {
 						text = "You're up to date.";
 					}
 					break;
-				case UpdatePanel.DOWNLOADING: 
+				case UpdatePanel.DOWNLOADING:
 					text = "Downloading update...";
 					break;
-				case UpdatePanel.DOWNLOAD_FAILED: 
+				case UpdatePanel.DOWNLOAD_FAILED:
 					text = "Downloading update failed.";
 					break;
-				case UpdatePanel.IMAGE_READY: 
+				case UpdatePanel.IMAGE_READY:
 					text = "Update downloaded.";
 					break;
-				case UpdatePanel.INSTALLING: 
+				case UpdatePanel.INSTALLING:
 					text = "Installing update... (will take a minute)";
 					break;
-				case UpdatePanel.INSTALLED: 
+				case UpdatePanel.INSTALLED:
 					text = "Update complete, please reconnect by connecting your device to the access point of your WiFi box and going to <a href='http://draw.doodle3d.com'>draw.doodle3d.com</a>";
 					//text = "Update complete, please <a href='javascript:location.reload(true);'>refresh Page</a>.";
 					break;
-				case UpdatePanel.INSTALL_FAILED: 
+				case UpdatePanel.INSTALL_FAILED:
 					text = "Installing update failed.";
 					break;
 			}
@@ -261,10 +261,10 @@ function UpdatePanel() {
 	}
 	this.updateInfoDisplay = function() {
 		var html = 'Current version: ' + self.currentVersion +
-			' (<a target="d3d-curr-relnotes" href="ReleaseNotes.html">release notes</a>). ';
+				' (<a target="d3d-curr-relnotes" href="ReleaseNotes.html">release notes</a>). ';
 		if(self.canUpdate) {
 			html += 'Latest version: ' + self.newestVersion +
-				' (<a target="d3d-new-relnotes" href="http://doodle3d.com/updates/images/ReleaseNotes.md">release notes</a>).';
+					' (<a target="d3d-new-relnotes" href="http://doodle3d.com/updates/images/ReleaseNotes.md">release notes</a>).';
 		}
 		self.infoDisplay.html(html);
 	}
