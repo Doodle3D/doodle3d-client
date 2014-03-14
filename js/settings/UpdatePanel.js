0acf442a4648f393928b452cf9dbaabcf9d072b3
peteruithoven <peter@peteruithoven.nl>
2014-03-14 12:23:57 +0100

    Corrected updated status text

--- js/settings/UpdatePanel.js
+++ js/settings/UpdatePanel.js
@@ -273,8 +273,8 @@ function UpdatePanel() {
 					text = "Installing update... (will take a minute)";
 					break;
 				case UpdatePanel.INSTALLED:
-					text = "Update complete, please reconnect by connecting your device to the access point of your WiFi box and going to <a href='http://draw.doodle3d.com'>draw.doodle3d.com</a>";
-					//text = "Update complete, please <a href='javascript:location.reload(true);'>refresh Page</a>.";
+					//text = "Update complete, please reconnect by connecting your device to the access point of your WiFi box and going to <a href='http://draw.doodle3d.com'>draw.doodle3d.com</a>";
+					text = "Update complete, please <a href='javascript:location.reload(true);'>refresh Page</a>.";
 					break;
 				case UpdatePanel.INSTALL_FAILED:
 					text = "Installing update failed.";
